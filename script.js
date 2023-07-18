bbb8d17ce9f26417b4d597feb68709bf92e6a668
ShadenAbdulah <sh22m2015@gmail.com>
2023-07-18 20:39:58 +0300

    enable shifting between tabs by JS

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 function alertFun() {
     window.alert('Sorry, we still working on this page.')
 }
+
+function shiftTaps(tapId, contentId) {
+    let toNonActive = document.getElementsByClassName('activeTap');
+    for (let i = 0; i < toNonActive.length; i++) {
+        toNonActive[i].classList.replace('activeTap', 'nonActiveTap');
+    }
+
+    let toHidden = document.getElementsByClassName('content');
+    for (let i = 0; i < toHidden.length; i++) {
+        toHidden[i].classList.replace('block', 'hidden');
+    }
+
+    let toActive = document.getElementById(tapId);
+    let content = document.getElementById(contentId);
+    toActive.classList.replace('nonActiveTap', 'activeTap');
+    content.classList.replace('hidden', 'block');
+}
+
+function rating(rate, text) {
+    let rateStr = document.getElementById(text);
+    rateStr.innerHTML = rate.toString();
+
+    let stars = document.getElementsByClassName(text);
+    for (let i = 0; i < rate; i++) {
+        stars[i].classList.replace('text-gray-300','text-yellow-500');
+    }
+    for (let i = rate; i < stars.length; i++) {
+        stars[i].classList.replace('text-yellow-500', 'text-gray-300');
+    }
+}
